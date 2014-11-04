0008a4be835a94f43e7b9236e1639a731502b716
Gregory Weinbach <gweinbach@gmail.com>
2014-11-04 18:58:08 +0100

    Post-condition is tested

--- ezmodel-core/src/test/scala/com/ezoky/ezmodel/actor/UseCaseClerkTest.scala
+++ ezmodel-core/src/test/scala/com/ezoky/ezmodel/actor/UseCaseClerkTest.scala
@@ -4,7 +4,7 @@ import akka.testkit.{ImplicitSender, TestKit}
 import com.ezoky.ezmodel.actor.TestConfig._
 import com.ezoky.ezmodel.actor.UseCaseClerk._
 import com.ezoky.ezmodel.core.Entities.{EntityState, Entity}
-import com.ezoky.ezmodel.core.UseCases.{Actor, Goal, PreCondition, UseCase}
+import com.ezoky.ezmodel.core.UseCases._
 import org.junit.runner.RunWith
 import org.scalatest.WordSpecLike
 import org.scalatest.junit.JUnitRunner
@@ -29,14 +29,18 @@ class UseCaseClerkTest
         val goal = Goal("a Goal")
         val test = useCaseClerk(actor,goal)
 
-        val entityState = EntityState(Entity("an entity"),"a state")
-
-        test ! AddPreCondition(actor,goal,entityState)
-        expectMsg(ConstrainedUseCase(PreCondition(UseCase(actor,goal),entityState)) )
-
         //within(1 second) {
-          //expectMsg(UseCaseCreated(UseCase(actor,goal)))
+        //expectMsg(UseCaseCreated(UseCase(actor,goal)))
         //}
+
+        val preCondition = EntityState(Entity("an entity"),"a pre-condition state")
+        val postCondition = EntityState(Entity("an entity"),"a post-condition state")
+
+        test ! AddPreCondition(actor,goal,preCondition)
+        expectMsg(ConstrainedUseCase(PreCondition(UseCase(actor,goal),preCondition)) )
+
+        test ! AddPostCondition(actor,goal,preCondition)
+        expectMsg(ConstrainedUseCase(PostCondition(UseCase(actor,goal),postCondition))) // equality of Use Cases is based on equality of their id, i.e. actor + goal
       }
       finally {
         system.shutdown()
