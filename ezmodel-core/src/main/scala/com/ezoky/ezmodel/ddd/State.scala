bcb4dfa9127d664d79669f6124e5d83c79fdca96
Gregory Weinbach <gweinbach@gmail.com>
2014-11-23 07:02:59 +0100

    No tests yet.

--- ezmodel-core/src/main/scala/com/ezoky/ezmodel/ddd/State.scala
+++ ezmodel-core/src/main/scala/com/ezoky/ezmodel/ddd/State.scala
@@ -7,11 +7,20 @@ trait AbstractState[S] {
 }
 
 object State {
+
   import scala.language.implicitConversions
 
-  implicit def implicitState[S](stateValue:S) = State(stateValue)
+  implicit def implicitState[S](stateValue: S) = State(stateValue)
+
+  def +(state1: State, state2: State): State = state2 match {
+    case NeutralState => state1
+    case _ => state1
+
 }
+
 case class State[S](stateValue: S) extends AbstractState[S]
+
+object NeutralState extends AbstractState[Any]
 
 object InitialState extends AbstractState[Any]
 
